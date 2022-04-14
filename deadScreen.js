1d0d858e52a78ddc7138ae17738586b1fdcb83e4
Joseph Thomas <74767414+joseph-beep@users.noreply.github.com>
2022-04-14 19:29:40 +0000

    Verwijder Death Screen

--- deadScreen.js
+++ deadScreen.js
@@ -1,15 +1,9 @@
 class DeadScreen {
   draw() {
-    text("Game Over Druk op [SPACE] verder gaan", 50, 50);
-
-    return 1;
+    return 0;
   }
 
   keyPressed() {
-    if (keyCode == 32) {
-      return 0;
-    }
-
-    return 1;
+    return 0;
   }
 }
